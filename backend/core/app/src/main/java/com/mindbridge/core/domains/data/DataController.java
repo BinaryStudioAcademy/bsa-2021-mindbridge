11cc31ec2e868a40340c07e80a4e4b6632af90c9
DmytroR <rudas72@gmail.com>
2021-07-28 17:37:17 +0300

    Added hint

--- backend/core/app/src/main/java/com/mindbridge/core/domains/data/DataController.java
+++ backend/core/app/src/main/java/com/mindbridge/core/domains/data/DataController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+// TODO: this is an example reference. Delete after getting familiar with the project structure
 @RestController
 @RequestMapping("/data")
 public class DataController {
