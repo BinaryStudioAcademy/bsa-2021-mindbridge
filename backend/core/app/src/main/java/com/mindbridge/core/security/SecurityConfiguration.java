1fba87631ab146a04fe12f02d5d7a29447d50155
kopot-o-d <oleg.kopot@binary-studio.com>
2021-07-28 17:19:28 +0300

    Add security configuration and backend dockerfile

---
+++ backend/core/app/src/main/java/com/mindbridge/core/security/SecurityConfiguration.java
@@ -0,0 +1,43 @@
+package com.mindbridge.core.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+
+@Configuration
+@EnableScheduling
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(
+	securedEnabled = true,
+	jsr250Enabled = true,
+	prePostEnabled = true
+)
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+			.headers()
+			.frameOptions()
+			.sameOrigin()
+			.and()
+
+			.cors()
+			.and()
+			.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+			.and().csrf().disable()
+			.formLogin().disable()
+			.httpBasic().disable()
+			.authorizeRequests()
+			.antMatchers("/auth/**", "/oauth2/**").permitAll()
+			.antMatchers("/swagger-ui/**").permitAll()
+			.anyRequest().authenticated()
+			.and();
+	}
+}
+
