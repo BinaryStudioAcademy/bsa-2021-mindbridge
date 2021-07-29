7f5543aac6ac1e1a7ee5e3530b51a3497dc1e1e8
AlenaGavriluk <alenagavriluk@inbox.ru>
2021-07-29 14:23:06 +0300

    Added Header component

--- frontend/src/screens/CreatePost/containers/CreatePostPage/index.tsx
+++ frontend/src/screens/CreatePost/containers/CreatePostPage/index.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import styles from './styles.module.scss';
 import classNames from 'classnames';
 import { connect } from 'react-redux';
+import Header from '@root/components/Header';
+import ProfileSidebar from '@root/components/ProfileSidebar';
 
 export interface ICreatePostProps extends IState, IActions {
 }
@@ -11,11 +13,17 @@ interface IState {
 
 interface IActions {
 }
+//use real value
+const notificationCount = 3;
 
 const CreatePost: React.FC<ICreatePostProps> = (
   { children }
 ) => (
-  <div className={classNames('content_wrapper', styles.container)}>create post</div>
+  <div className={classNames('content_wrapper', styles.container)}>
+    <Header notificationCount={notificationCount}/>
+    <ProfileSidebar/>
+    create post
+  </div>
 );
 
 const mapStateToProps: (state) => IState = state => ({
