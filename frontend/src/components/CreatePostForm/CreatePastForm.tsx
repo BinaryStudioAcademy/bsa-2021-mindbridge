ecc65dee20038c9ae9e03ca00c7512229111693f
AlenaGavriluk <alenagavriluk@inbox.ru>
2021-08-03 15:53:00 +0300

    fixed styles in form

--- frontend/src/components/CreatePostForm/CreatePastForm.tsx
+++ frontend/src/components/CreatePostForm/CreatePastForm.tsx
@@ -1,29 +1,27 @@
 import React from 'react';
 import styles from './styles.module.scss';
-import Image_1 from './svg/svg1';
-import Image_2 from './svg/svg2';
+import Image1 from './svg/svg1';
+import Image2 from './svg/svg2';
 
-function CreatePostForm(props) {
+function CreatePostForm() {
   return (
     <form className={styles.create_post_form}>
-        
-    <label className={styles.file_input_rectangle}>
-      <Image_1/>
-      <span>Add a cover image</span>
-      <input className={styles.invisible} type="file"></input>
-    </label>
-    <input type="text" placeholder="Enter the title of the article"></input>
-    <div className={styles.content_input_container}>
-      <label className={styles.file_input_round}>
-        <Image_2/>
-        <input className={styles.invisible} type="file"></input>
+      <label className={styles.file_input_rectangle}>
+        <Image1 />
+        <span>Add a cover image</span>
+        <input className={styles.invisible} type="file" />
       </label>
-      <textarea className={styles.content_input} placeholder="Write your post content"></textarea>
-    </div>
-    <input  type="text" placeholder="Enter tags"></input>
-  </form>
-  )
+      <input type="text" placeholder="Enter the title of the article" />
+      <div className={styles.content_input_container}>
+        <label className={styles.file_input_round}>
+          <Image2 />
+          <input className={styles.invisible} type="file" />
+        </label>
+        <textarea className={styles.content_input} placeholder="Write your post content" />
+      </div>
+      <input type="text" placeholder="Enter tags" />
+    </form>
+  );
 }
 
 export default CreatePostForm;
-
