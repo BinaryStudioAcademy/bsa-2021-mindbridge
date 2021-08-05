dea7102d2e5250bc213bf6d3b43b12c9d3943500
kopot-o-d <oleg.kopot@binary-studio.com>
2021-08-05 17:10:06 +0300

    Remove test module, add swagger host and auth

--- backend/core/app/src/main/java/com/mindbridge/core/CoreApplication.java
+++ backend/core/app/src/main/java/com/mindbridge/core/CoreApplication.java
@@ -36,6 +36,4 @@ public class CoreApplication {
 		return executor;
 	}
 
-
-
 }
--- backend/core/app/src/main/java/com/mindbridge/core/configs/SwaggerConfig.java
+++ backend/core/app/src/main/java/com/mindbridge/core/configs/SwaggerConfig.java
@@ -1,36 +1,61 @@
 package com.mindbridge.core.configs;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.paths.RelativePathProvider;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import javax.servlet.ServletContext;
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
+
 	private final ServletContext servletContext;
+
+	@Value("${swagger.host}")
+	private String swaggerHost;
 
 	@Autowired
 	public SwaggerConfig(ServletContext servletContext) {
 		this.servletContext = servletContext;
 	}
 
+	private ApiKey apiKey() {
+		return new ApiKey("JWT", "Authorization", "header");
+	}
+
+	private SecurityContext securityContext() {
+		return SecurityContext.builder().securityReferences(defaultAuth()).build();
+	}
+
+	private List<SecurityReference> defaultAuth() {
+		AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+		AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+		authorizationScopes[0] = authorizationScope;
+		return Arrays.asList(new SecurityReference("JWT", authorizationScopes));
+	}
+
 	@Bean
 	public Docket api() {
 		return new Docket(DocumentationType.SWAGGER_2)
-			.pathProvider(new RelativePathProvider(servletContext) {
-				@Override
-				public String getApplicationBasePath() {
-					return "/api";
-				}
-			})
-			.select().apis(RequestHandlerSelectors.any()).paths(PathSelectors.any()).build();
+			.host(swaggerHost)
+			.securityContexts(Arrays.asList(securityContext()))
+			.securitySchemes(Arrays.asList(apiKey()))
+			.select()
+			.apis(RequestHandlerSelectors.any())
+			.paths(PathSelectors.any()).build();
 	}
+
 }
