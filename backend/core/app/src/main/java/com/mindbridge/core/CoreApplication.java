b7717b94f686b2b608080c6da70414405709fa77
kopot-o-d <oleg.kopot@binary-studio.com>
2021-08-05 13:44:38 +0300

    Fix swagger and web sockets

--- backend/core/app/src/main/java/com/mindbridge/core/CoreApplication.java
+++ backend/core/app/src/main/java/com/mindbridge/core/CoreApplication.java
@@ -9,9 +9,11 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import springfox.documentation.PathProvider;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.paths.DefaultPathProvider;
 import springfox.documentation.spring.web.plugins.Docket;
 
 import java.util.concurrent.Executor;
@@ -39,8 +41,12 @@ public class CoreApplication {
 
 	@Bean
 	public Docket api() {
-		return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.any())
-				.paths(PathSelectors.any()).build();
+		return new Docket(DocumentationType.SWAGGER_2).pathProvider(new DefaultPathProvider() {
+			@Override
+			public String getOperationPath(String operationPath) {
+				return "/api";
+			}
+		}).select().apis(RequestHandlerSelectors.any()).paths(PathSelectors.any()).build();
 	}
 
 }
