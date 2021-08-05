1c02cf9422a93bfe2680bbfd5a607629429d79e2
Кирилл <glum.kiril@gmail.com>
2021-08-05 13:56:04 +0300

    Backend linter check

--- backend/core/app/src/main/java/com/mindbridge/core/domains/user/dto/UserDto.java
+++ backend/core/app/src/main/java/com/mindbridge/core/domains/user/dto/UserDto.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.mindbridge.core.domains.user.dto;
 
 import lombok.Getter;
@@ -21,21 +20,3 @@ public class UserDto {
 	private String lastName;
 
 }
-=======
-package com.mindbridge.core.domains.user.dto;
-
-import lombok.Getter;
-import lombok.Setter;
-
-import java.util.UUID;
-
-@Getter
-@Setter
-public class UserDto {
-	private UUID id;
-	private String email;
-	private String firstName;
-	private String lastName;
-	private boolean emailVerified;
-}
->>>>>>> ff40577a61e4fb36ca184a81fc852805286a3c44
