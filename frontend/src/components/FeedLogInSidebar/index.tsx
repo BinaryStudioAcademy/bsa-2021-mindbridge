dd21460bcdc54e1799925bb17cc15939ee1cae15
Кирилл <glum.kiril@gmail.com>
2021-08-06 13:18:25 +0300

    redirection bug fix

--- frontend/src/components/FeedLogInSidebar/index.tsx
+++ frontend/src/components/FeedLogInSidebar/index.tsx
@@ -1,25 +1,25 @@
 import React, { FunctionComponent } from 'react';
 import styles from './styles.module.scss';
-import { useHistory } from 'react-router-dom';
 
-const FeedLogInSidebar: FunctionComponent = () => {
-  const history = useHistory();
-  return (
-    <div className={styles.logInSideBar}>
-      <div className={styles.title}>
-        MindBridge is a community of 500.999 amazing developers
-      </div>
-      <div className={styles.description}>
-        We&apos;re a place where coders share, stay up-to-date and grow their careers.
-      </div>
-      <button type="button" onClick={() => history.push('/registration')} className={`${styles.dark_button}`}>
+const FeedLogInSidebar: FunctionComponent = () => (
+  <div className={styles.logInSideBar}>
+    <div className={styles.title}>
+      MindBridge is a community of 500.999 amazing developers
+    </div>
+    <div className={styles.description}>
+      We&apos;re a place where coders share, stay up-to-date and grow their careers.
+    </div>
+    <a href="/registration">
+      <button type="button" className={`${styles.dark_button}`}>
         Create new account
       </button>
-      <button type="button" onClick={() => history.push('/login')} className={styles.darkBorderButton}>
+    </a>
+    <a href="/login">
+      <button type="button" className={styles.darkBorderButton}>
         Log in
       </button>
-    </div>
-  );
-};
+    </a>
+  </div>
+);
 
 export default FeedLogInSidebar;
