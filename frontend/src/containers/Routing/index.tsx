c733dba2cf6b6297fe658a56898f214d4d701d62
COOcDTEE <grifyshow@gmail.com>
2021-08-06 11:39:55 +0300

    run front lint

--- frontend/src/containers/Routing/index.tsx
+++ frontend/src/containers/Routing/index.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { Redirect, Route, Switch } from 'react-router-dom';
 import LoaderWrapper from 'components/LoaderWrapper';
 import PublicRoute from 'components/PublicRoute';
