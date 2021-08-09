dc16004b0bf233fe9db70ae92d45adf447770c55
AlenaGavriluk <alenagavriluk@inbox.ru>
2021-08-09 15:20:17 +0300

    Tags backend

---
+++ backend/core/app/src/main/java/com/mindbridge/core/domains/tag/TagController.java
@@ -0,0 +1,27 @@
+package com.mindbridge.core.domains.tag;
+
+import com.mindbridge.core.domains.tag.dto.TagDto;
+import com.mindbridge.core.domains.tag.dto.TagService;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("tag")
+@Validated
+public class TagController {
+	private final TagService tagService;
+
+	@Autowired
+	public TagController(TagService tagService) {
+		this.tagService = tagService;
+	}
+
+	@GetMapping("/all")
+	public List<TagDto> getTags() {
+		return tagService.getTags();
+	}
+}
--- backend/core/app/src/main/java/com/mindbridge/core/domains/tag/dto/TagDto.java
+++ backend/core/app/src/main/java/com/mindbridge/core/domains/tag/dto/TagDto.java
@@ -1,15 +1,21 @@
 package com.mindbridge.core.domains.tag.dto;
 
 import com.mindbridge.data.domains.tag.model.Tag;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import java.util.UUID;
 
 @Data
+@AllArgsConstructor
 public class TagDto {
 
 	private UUID id;
 
 	private String name;
+
+	public static TagDto fromEntity(Tag tag){
+		return new TagDto(tag.getId(), tag.getName());
+	}
 
 }
