5cf4d70fbadd4c9070ca9bbe271c979e707dba7b
Vadym Deputat <vadikdeputat@gmail.com>
2021-08-11 18:09:20 +0200

    Changes after code review

--- frontend/src/screens/NotFound/components/Btn404/index.tsx
+++ frontend/src/screens/NotFound/components/Btn404/index.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Button } from 'semantic-ui-react';
 import style from './styles.module.scss';
+import { history } from '@helpers/history.helper';
 
 const ButtonEmphasis = () => (
   <div>
-    <Button onClick={() => window.location.replace('/')} className={style.buttonRight}>Go to homepage</Button>
+    <Button onClick={() => history.push('/')} className={style.buttonRight}>Go to homepage</Button>
   </div>
 );
 
