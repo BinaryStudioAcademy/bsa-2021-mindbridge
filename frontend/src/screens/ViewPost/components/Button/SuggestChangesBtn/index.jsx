d928aee9e17cdce08de301dbf3ce7899f944c550
Vadym Deputat <vadikdeputat@gmail.com>
2021-08-12 22:55:08 +0200

    Removing unused files

--- frontend/src/screens/ViewPost/components/Button/SuggestChangesBtn/index.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { Button } from 'semantic-ui-react';
-import styles from './styles.module.scss';
-
-const SuggestChangesBtn = () => <Button className={styles.btnSuggest}>Suggest Change</Button>;
-
-export default SuggestChangesBtn;
