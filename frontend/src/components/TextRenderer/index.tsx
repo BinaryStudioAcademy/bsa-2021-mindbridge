fe5237771bb89938dfaa4af53e93d87fed65042c
AlenaGavriluk <alenagavriluk@inbox.ru>
2021-08-12 16:03:26 +0300

    add html parser and reactMarkdown

--- frontend/src/components/TextRenderer/index.tsx
+++ frontend/src/components/TextRenderer/index.tsx
@@ -1,18 +1,22 @@
 import React from 'react';
 import styles from './styles.module.scss';
 import classNames from 'classnames';
-import marked from 'marked';
+import parse from 'html-react-parser';
+import ReactMarkdown from 'react-markdown';
 
-const TextRender = ({ className, content, markdown }) => (
-
-  <div
-    className={classNames(styles.rendered_text, className)}
-    dangerouslySetInnerHTML={
-      markdown
-        ? { __html: marked(content) }
-        : { __html: content }
-    }
-  />
-);
+const TextRender = ({ className, content, markdown }) => {
+  if (markdown) {
+    return (
+      <ReactMarkdown className={classNames(styles.rendered_text, className)}>
+        {content}
+      </ReactMarkdown>
+    );
+  }
+  return (
+    <div className={classNames(styles.rendered_text, className)}>
+      {parse(content)}
+    </div>
+  );
+};
 
 export default TextRender;
