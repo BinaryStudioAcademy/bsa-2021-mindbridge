0eaec651abb0a47b06e1127fd2527fa2f49140f9
AlenaGavriluk <alenagavriluk@inbox.ru>
2021-08-24 23:56:10 +0300

    Don't show closed and accepted PR on sidebar

--- frontend/src/screens/ViewPost/models/IContribution.ts
+++ frontend/src/screens/ViewPost/models/IContribution.ts
@@ -1,7 +1,9 @@
+import { PrState } from '@root/screens/PullRequest/models/IPostPR';
 import { IUser } from '@screens/ViewPost/models/IUser';
 
 export interface IContribution {
   id: string;
   author: IUser;
   createdAt: string;
+  state: PrState;
 }
