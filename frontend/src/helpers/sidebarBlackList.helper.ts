996ac50d3923b1ef17962ce728f39920f47c538f
COOcDTEE <grifyshow@gmail.com>
2021-08-25 15:44:40 +0300

    show sidebar on pullRequest page

--- frontend/src/helpers/sidebarBlackList.helper.ts
+++ frontend/src/helpers/sidebarBlackList.helper.ts
@@ -1,7 +1,7 @@
 import { history } from '@helpers/history.helper';
 
 export const checkSidebarShown = () => {
-  const sidebarBlackList = ['/login', '/registration', '/pullRequest'];
+  const sidebarBlackList = ['/login', '/registration'];
 
   return sidebarBlackList.every(item => !history.location.pathname.startsWith(item));
 };
