93a2778bc0925da3c42c8de7100246d62e18f2cf
COOcDTEE <grifyshow@gmail.com>
2021-08-25 19:17:03 +0300

    add pop up

--- frontend/src/components/CreatePostForm/CreatePostForm.tsx
+++ frontend/src/components/CreatePostForm/CreatePostForm.tsx
@@ -210,7 +210,14 @@ const CreatePostForm: React.FC<ICreatePostFormProps> = (
       />
 
       {dropzoneOrTag}
-      <TagsDropdown onChange={handleTags} data={form.tags} allTags={allTags} />
+      <Popup
+        trigger={(
+          <TagsDropdown onChange={handleTags} data={form.tags} allTags={allTags} />
+        )}
+        content="Max amount of tags has reached"
+        open={form.tags.length === 5}
+        position="left center"
+      />
     </div>
   );
 };
