17f37647a8c54ad2c2e2af1aa545b3251d7ddafb
COOcDTEE <grifyshow@gmail.com>
2021-08-25 18:32:50 +0300

    add private route

--- frontend/src/components/PrivateRoute/index.tsx
+++ frontend/src/components/PrivateRoute/index.tsx
@@ -1,24 +1,46 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Route, Redirect } from 'react-router-dom';
 import { ACCESS_TOKEN } from '@screens/Login/constants/auth_constants';
+import {checkSidebarShown} from "@helpers/sidebarBlackList.helper";
+import {history} from "@helpers/history.helper";
+import Sidebar from "@screens/Sidebar/containers/SidebarPage";
+import styles from './styles.module.scss';
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-const PrivateRoute = ({ component: Component, ...rest }) => (
-  <Route
-    {...rest}
-    render={props => {
-      // get token
-      const token = localStorage.getItem(ACCESS_TOKEN);
-      if (!token) {
-        return (
-          <Redirect
-            to={{ pathname: '/login', state: { from: props.location } }}
-          />
-        );
-      }
-      return <Component {...props} />;
-    }}
-  />
-);
+const PrivateRoute = ({ component: Component, ...rest }) => {
+  const [isSidebarShown, setIsSidebarShown] = useState(checkSidebarShown());
+  history.listen(() => {
+    setIsSidebarShown(checkSidebarShown());
+  });
+
+  return (
+    <Route
+      {...rest}
+      render={props => {
+        // get token
+        const token = localStorage.getItem(ACCESS_TOKEN);
+        if (!token) {
+          return (
+            <Redirect
+              to={{ pathname: '/login', state: { from: props.location } }}
+            />
+          );
+        }
+        if (isSidebarShown) {
+          return (
+            <div className={styles.wrapper}>
+              <Component {...props} />
+              <Sidebar />
+            </div>
+          );
+        } else {
+          return (
+            <Component {...props} />
+          );
+        }
+      }}
+    />
+  );
+};
 
 export default PrivateRoute;
