70c2951f491913b37499aa61b46dc3535f67f8b0
nickolay-grechkin <nickolaygrechkin@gmail.com>
2021-08-27 11:57:20 +0300

    fix icons on feed page

--- frontend/src/screens/ViewPost/components/ViewPostCard/index.tsx
+++ frontend/src/screens/ViewPost/components/ViewPostCard/index.tsx
@@ -30,86 +30,88 @@ const ViewPostCard: FunctionComponent<IViewPostCardProps> = ({ post, isAuthor, h
   };
 
   return (
-    <Card className={styles.viewCard}>
-      <div className={styles.cardContent}>
-        <Card.Content>
-          <Feed>
-            <div className={styles.gridColumn}>
-              <div className={styles.leftSide}>
-                <div className={styles.bgCircle}>
-                  <div className={styles.ratingComponent}>
-                    <RatingComponent
-                      postRating={post.rating}
-                      handleLikePost={handleLikePost}
-                      handleDisLikePost={handleDisLikePost}
-                      postId={post.id}
-                      userInfo={userInfo}
-                      arrowUpColor={userInfo.userReactions.find(postReaction => postReaction.postId === post.id
-                        && postReaction.liked)
-                        ? ('#8AC858'
-                        ) : (
-                          '#66B9FF'
-                        )}
-                      arrowDownColor={userInfo.userReactions.find(postReaction => postReaction.postId === post.id
-                        && !postReaction.liked)
-                        ? ('#F75C48'
-                        ) : (
-                          '#66B9FF'
-                        )}
-                    />
+    <div className={styles.container}>
+      <Card className={styles.viewCard}>
+        <div className={styles.cardContent}>
+          <Card.Content>
+            <Feed>
+              <div className={styles.gridColumn}>
+                <div className={styles.leftSide}>
+                  <div className={styles.bgCircle}>
+                    <div className={styles.ratingComponent}>
+                      <RatingComponent
+                        postRating={post.rating}
+                        handleLikePost={handleLikePost}
+                        handleDisLikePost={handleDisLikePost}
+                        postId={post.id}
+                        userInfo={userInfo}
+                        arrowUpColor={userInfo.userReactions.find(postReaction => postReaction.postId === post.id
+                          && postReaction.liked)
+                          ? ('#8AC858'
+                          ) : (
+                            '#66B9FF'
+                          )}
+                        arrowDownColor={userInfo.userReactions.find(postReaction => postReaction.postId === post.id
+                          && !postReaction.liked)
+                          ? ('#F75C48'
+                          ) : (
+                            '#66B9FF'
+                          )}
+                      />
+                    </div>
                   </div>
+                  <div className={styles.bgCircle}>
+                    <FavouriteSvg />
+                  </div>
+                  <div className={styles.bgCircle}>
+                    <CommentSvg />
+                  </div>
+                  <div className={styles.bgCircle}>
+                    <ShareSvg />
+                  </div>
+                  {isAuthor && (
+                  <div role="button" tabIndex={0} className={styles.bgCircle} onKeyDown={goToEdit} onClick={goToEdit}>
+                    <EditSvg />
+                  </div>
+                  )}
                 </div>
-                <div className={styles.bgCircle}>
-                  <FavouriteSvg />
-                </div>
-                <div className={styles.bgCircle}>
-                  <CommentSvg />
-                </div>
-                <div className={styles.bgCircle}>
-                  <ShareSvg />
-                </div>
-                {isAuthor && (
-                <div role="button" tabIndex={0} className={styles.bgCircle} onKeyDown={goToEdit} onClick={goToEdit}>
-                  <EditSvg />
-                </div>
-                )}
-              </div>
-              <img
-                className={styles.image}
-                src={post.coverImage ?? 'https://i.imgur.com/KVI8r34.jpg'}
-                alt="media"
-              />
-            </div>
-
-            <div className={styles.postName}>{post.title}</div>
-            <div className={styles.btnWrapper}>
-              {post.tags.map(tag => (
-                <TagsMenu
-                  key={tag.id}
-                  tag={tag.name}
+                <img
+                  className={styles.image}
+                  src={post.coverImage ?? 'https://i.imgur.com/KVI8r34.jpg'}
+                  alt="media"
                 />
-              ))}
-            </div>
-            <div className={styles.cardHeader}>
-              <PostInformation
-                id={post.author.id}
-                nickname={post.author.nickname}
-                date={post.createdAt}
-                avatar={post.author.avatar}
-                readTime={readingTime(post.text).text}
+              </div>
+
+              <div className={styles.postName}>{post.title}</div>
+              <div className={styles.btnWrapper}>
+                {post.tags.map(tag => (
+                  <TagsMenu
+                    key={tag.id}
+                    tag={tag.name}
+                  />
+                ))}
+              </div>
+              <div className={styles.cardHeader}>
+                <PostInformation
+                  id={post.author.id}
+                  nickname={post.author.nickname}
+                  date={post.createdAt}
+                  avatar={post.author.avatar}
+                  readTime={readingTime(post.text).text}
+                />
+              </div>
+            </Feed>
+            <div className={styles.postBody}>
+              <TextRenderer
+                className={styles.content}
+                markdown={post.markdown}
+                content={post.text}
               />
             </div>
-          </Feed>
-          <div className={styles.postBody}>
-            <TextRenderer
-              className={styles.content}
-              markdown={post.markdown}
-              content={post.text}
-            />
-          </div>
-        </Card.Content>
-      </div>
-    </Card>
+          </Card.Content>
+        </div>
+      </Card>
+    </div>
   );
 };
 
