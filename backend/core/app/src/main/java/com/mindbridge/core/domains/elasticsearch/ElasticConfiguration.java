2006f33af9277331d2f301ab1fed4b28b28891cc
Vadym Deputat <vadikdeputat@gmail.com>
2021-08-31 17:58:05 +0200

    deleted unused imports

--- backend/core/app/src/main/java/com/mindbridge/core/domains/elasticsearch/ElasticConfiguration.java
+++ backend/core/app/src/main/java/com/mindbridge/core/domains/elasticsearch/ElasticConfiguration.java
@@ -12,8 +12,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.elasticsearch.client.ClientConfiguration;
-import org.springframework.data.elasticsearch.client.RestClients;
 import org.springframework.data.elasticsearch.config.ElasticsearchConfigurationSupport;
 import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
 import org.springframework.data.elasticsearch.core.ElasticsearchRestTemplate;
