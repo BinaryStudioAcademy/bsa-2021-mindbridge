06d86a67d514ae52ad319b3d8bdc4af44d9ef9c9
Vadym Deputat <vadikdeputat@gmail.com>
2021-08-31 16:58:00 +0200

    add elasticsearch to staging

--- backend/core/app/src/main/java/com/mindbridge/core/domains/elasticsearch/ElasticConfiguration.java
+++ backend/core/app/src/main/java/com/mindbridge/core/domains/elasticsearch/ElasticConfiguration.java
@@ -1,5 +1,12 @@
 package com.mindbridge.core.domains.elasticsearch;
 
+import org.apache.http.HttpHost;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestClientBuilder;
 import org.elasticsearch.client.RestHighLevelClient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -20,11 +27,26 @@ public class ElasticConfiguration extends ElasticsearchConfigurationSupport {
 	@Value("${elasticsearch.host}")
 	private String host;
 
+	@Value("${elasticsearch.user}")
+	private String userName;
+
+	@Value("${elasticsearch.password}")
+	private String password;
+
+	@Value("${elasticsearch.port}")
+	private int port;
+
 	@Bean
 	public RestHighLevelClient client() {
-		ClientConfiguration clientConfiguration = ClientConfiguration.builder().connectedTo(host).build();
+		final CredentialsProvider credentialsProvider = new BasicCredentialsProvider();
+		credentialsProvider.setCredentials(AuthScope.ANY,
+			new UsernamePasswordCredentials(userName, password));
 
-		return RestClients.create(clientConfiguration).rest();
+		RestClientBuilder builder = RestClient.builder(new HttpHost(host, port, "https"))
+			.setHttpClientConfigCallback(httpClientBuilder -> httpClientBuilder.setDefaultCredentialsProvider(credentialsProvider));
+
+		RestHighLevelClient client = new RestHighLevelClient(builder);
+		return client;
 	}
 
 	@Bean
