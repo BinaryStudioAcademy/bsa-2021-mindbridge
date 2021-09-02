cc76090154d5f931efb3a78002516d48ec4bac48
Vadym Deputat <vadikdeputat@gmail.com>
2021-09-02 18:27:06 +0200

    mock mailsender

--- backend/core/app/src/main/java/com/mindbridge/core/domains/helpers/MailSender.java
+++ backend/core/app/src/main/java/com/mindbridge/core/domains/helpers/MailSender.java
@@ -2,11 +2,13 @@ package com.mindbridge.core.domains.helpers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Profile;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 @Service
+@Profile("DEV")
 public class MailSender {
 	@Autowired
 	private JavaMailSender emailSender;
