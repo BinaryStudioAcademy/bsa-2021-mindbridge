f7e10d058f4787bb68c3511ca007200fc6b9d8cd
AlenaGavriluk <alenagavriluk@inbox.ru>
2021-09-10 13:27:19 +0300

    message

--- frontend/src/screens/Login/containers/sagas.ts
+++ frontend/src/screens/Login/containers/sagas.ts
@@ -67,7 +67,7 @@ function* getUserIp() {
     const res: any = yield call(userIp);
     yield put(getUserIpRoutine.success(res.data.IPv4));
   } catch (ex) {
-    yield put(getUserIpRoutine.failure(ex.massage));
+    yield put(getUserIpRoutine.failure(ex.message));
   }
 }
 
